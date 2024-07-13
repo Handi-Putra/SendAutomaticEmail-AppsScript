ee0dd6750041ca0432372598843c387c78eda18c
Handi <43770290+Handi-Putra@users.noreply.github.com>
2024-07-13 18:51:49 +0700

    Update sendAutomatedEmail.js

--- sendAutomatedEmail.js
+++ sendAutomatedEmail.js
@@ -1,25 +1,28 @@
 function sendAutomatedEmail() {
   var sheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName('Testing Send Automatic Email');
 
-  var recipient = "INPUT TO MAIL HERE"; // Replace with the recipient's email address
-  var ccRecipient = "INPUT CC MAIL HERE"; // Replace with the actual CC email address
+  var recipient = sheet.getRange("G2:G4"); // email to
+  var ccRecipient = sheet.getRange("H2:H4"); // email cc
 
-  var dateRange = sheet.getRange("B2:4"); // Date
+  var dateRange = sheet.getRange("B2:B4"); // Date
   var subjectRange = sheet.getRange("J2:J4"); // Subject
+  var bodyRange = sheet.getRange("I2:I4"); // body
 
   // Get the values from the specified ranges
   var dateValues = dateRange.getValues();
   var subjectValues = subjectRange.getValues();
+  var bodyValues = bodyRange.getValues();
 
   for (var i = 0; i < dateValues.length; i++) {
     var date = dateValues[i][0];
     var subject = subjectValues[i][0];
+    var bodyEmail = bodyValues[i][0];
     
     if (date && subject) {
       var formattedDate = formatDate(date);
       var subjectEmail = subject;
-      var body = "Dengan Hormat Pak " + recipient + ",\n\n" +
-                 "hanya Sekedar testing saja\n";
+      var body = "Dengan Hormat Pak " + recipient + ",\n\n" + bodyEmail +
+                 "\n";
 
       // Schedule the email to be sent
       var today = new Date();
