a1a8d77f8ce27d5f221e8de43d9f0922c2403b87
Handi <43770290+Handi-Putra@users.noreply.github.com>
2024-07-13 18:57:38 +0700

    Update sendAutomatedEmail.js

--- sendAutomatedEmail.js
+++ sendAutomatedEmail.js
@@ -1,24 +1,27 @@
 function sendAutomatedEmail() {
   var sheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName('Testing Send Automatic Email');
 
-  var recipient = sheet.getRange("G2:G4"); // email to
-  var ccRecipient = sheet.getRange("H2:H4"); // email cc
-
+  var recipientRange = sheet.getRange("G2:G4"); // email to
+  var ccRecipientRange = sheet.getRange("H2:H4"); // email cc
   var dateRange = sheet.getRange("B2:B4"); // Date
   var subjectRange = sheet.getRange("J2:J4"); // Subject
   var bodyRange = sheet.getRange("I2:I4"); // body
 
   // Get the values from the specified ranges
-  var dateValues = dateRange.getValues();
-  var subjectValues = subjectRange.getValues();
-  var bodyValues = bodyRange.getValues();
+  var recipientValues = recipientRange.getValues(); // Value email To
+  var ccRecipientValues = ccRecipientRange.getValues(); // Value email CC
+  var dateValues = dateRange.getValues(); // Value Send Date email
+  var subjectValues = subjectRange.getValues(); // Value Subject email
+  var bodyValues = bodyRange.getValues(); // Value Body email
 
   for (var i = 0; i < dateValues.length; i++) {
     var date = dateValues[i][0];
+    var recipient = recipientValues[i][0];
+    var ccRecipient = ccRecipientValues[i][0];
     var subject = subjectValues[i][0];
     var bodyEmail = bodyValues[i][0];
     
-    if (date && subject) {
+    if (date && subject && recipient) {
       var formattedDate = formatDate(date);
       var subjectEmail = subject;
       var body = "Dengan Hormat Pak " + recipient + ",\n\n" + bodyEmail +
